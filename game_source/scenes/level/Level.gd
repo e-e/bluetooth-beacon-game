bbc7e6df13bcc6c60fd1a2968295efbf0c6a2065
Eric Ellingson <god@eric.wtf>
2019-09-22 23:53:45 -0700

    name and queue_free()

--- game_source/scenes/level/Level.gd
+++ game_source/scenes/level/Level.gd
@@ -17,8 +17,10 @@ func _on_Player_level_completed():
 	current_level = min(current_level, FINISHED)
 	var current_level_node = $CurrentLevel
 	remove_child(current_level_node)
+	current_level_node.queue_free()
 	
 	if current_level == SEARCHING:
 		var next_level_resource = load("res://scenes/level/levels/ExploreLevel.tscn")
 		var next_level = next_level_resource.instance()
+		next_level.name = "CurrentLevel"
 		add_child(next_level)
